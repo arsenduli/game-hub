93f6bad878294922a28a0558f51b8c8135bbc78f
arsenduli <119952813+arsenduli@users.noreply.github.com>
2023-06-13 10:55:38 +0200

    Build a responsive layuot

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { Button, ButtonGroup } from "@chakra-ui/react";
-
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 function App() {
-  return <Button colorScheme={"blue"}>Press</Button>;
+  return (
+    <Grid
+      templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
+    >
+      <GridItem area={"nav"} bg={"coral"}>
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area={"aside"} bg={"dodgerblue"}>
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area={"main"} bg={"gold"}>
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
