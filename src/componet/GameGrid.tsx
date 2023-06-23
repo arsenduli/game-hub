6b5349790eb70083b5d35c54a74c2f2b7946fe54
arsenduli <119952813+arsenduli@users.noreply.github.com>
2023-06-23 16:06:05 +0200

    Refactor GAme Grid

--- src/componet/GameGrid.tsx
+++ src/componet/GameGrid.tsx
@@ -17,13 +17,10 @@ interface Props {
 const GameGrid = ({gameQuery} : Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeleton = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+  if(error) return <Text>{error}</Text>
   return (
-    <>
-      {error && (
-        <Text fontSize={"100px"} color={"red"}>
-          {error}
-        </Text>
-      )}
+  
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding="10px"
@@ -41,7 +38,6 @@ const GameGrid = ({gameQuery} : Props) => {
           </GameCardContainer>
         ))}
       </SimpleGrid>
-    </>
   );
 };
 
